541a5a3baf9c0deeaea35c3ed42f51c3ac974af5
Cauã Belodi <115277406+Cauabc@users.noreply.github.com>
2023-02-22 15:11:05 -0300

    All done, version of SDK - 3.1.2

---
+++ SmartSchool-WebAPI/Data/IRepository.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using SmartSchool_WebAPI.Models;
+
+namespace SmartSchool_WebAPI.Data
+{
+    public interface IRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveChangesAsync();
+
+        Task<Aluno[]> GetAllAlunosAsync(bool includeProfessor);        
+        Task<Aluno[]> GetAlunosAsyncByDisciplinaId(int disciplinaId, bool includeDisciplina);
+        Task<Aluno> GetAlunoAsyncById(int alunoId, bool includeProfessor);
+        
+        Task<Professor[]> GetAllProfessoresAsync(bool includeAluno);
+        Task<Professor> GetProfessorAsyncById(int professorId, bool includeAluno);
+        Task<Professor[]> GetProfessoresAsyncByAlunoId(int alunoId, bool includeDisciplina);
+    }
+}
